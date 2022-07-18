3bf0b137f31146bd45867a4ea9ff2f4c021b124f
Danny Diaz <daniel.diaz.oem@gmail.com>
2022-07-18 00:13:32 -0400

    Use less, and made gpg --armor --export

--- linux_install.sh
+++ linux_install.sh
@@ -28,8 +28,8 @@ echo "DONE";
 
 echo "Getting .(dot)-files...";
 	git clone -b debian --bare git@github.com:dannydiaz17/linux-config.git $HOME/.dot
-	$dot remote add origin git git@github.com:dannydiaz17/linux-config.git
-	$dot branch --set-upstream-to=origin/debian
+	$dot remote add -t debian origin git@github.com:dannydiaz17/linux-config.git
+	$dot branch --set-upstream-to=debian
 	$dot checkout
 	gopass git clone git@github.com:dannydiaz/gopass.git ~/.password-store/.git
 	gopass git remote add origin git@github.com:dannydiaz17/gopass.git; 
@@ -43,16 +43,13 @@ echo "Setting up Git Auth...";
 
 	curl -LO https://raw.githubusercontent.com/GitCredentialManager/git-credential-manager/main/src/linux/Packaging.Linux/install-from-source.sh && sh ./install-from-source.sh && git-credential-manager-core configure
 	ssh-keygen -t ed25519 -C "daniel.diaz.oem@gmail.com";
-	cat $HOME/.ssh/id_ed25519.pub; echo "Add this Public Key to GitHub";
-	eval "$(ssh-agent -s)"; ssh-add ~/.ssh/id_ed25519; sleep 2; echo "\n";
-	gpg --gen-key; gpg --list-secret-keys --keyid-format=long; 
-	echo "Copy Secret Key and paste after the following command";
-	echo 	"\n gpg --armor --export {SECRET_KEY}\n" \
-		"Take your generated PGP Public Key BLOCK to GitHub";
-
+	less $HOME/.ssh/id_ed25519.pub; echo "Add this Public Key to GitHub";
+	eval "$(ssh-agent -s)"; ssh-add ~/.ssh/id_ed25519; sleep 2; 
+	gpg --gen-key; echo "Secret Key?";
+	gpg --list-secret-keys --keyid-format=long | less;
+	read KEY; gpg --armor --export $KEY
 
 # Changes "$SHELL" to ZSH
 
 echo "Changing Shell...";
-	chsh -s /usr/bin/zsh; /usr/bin/zsh;
-echo "Enjoy!";
+	chsh -s /usr/bin/zsh; echo "Enjoy!"; /usr/bin/zsh;
